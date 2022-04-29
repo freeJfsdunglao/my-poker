0bde59dd0ac2104c1abe5dee8e22f494e859ff1a
John sa Bahay <dev@masterspoker.ph>
2022-04-29 17:22:04 +0800

    randomizer with scheduler done

---
+++ src/randomizer/randomizer.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { RandomizerService } from './randomizer.service';
+
+@Module({
+  providers: [RandomizerService],
+  exports: [RandomizerService],
+})
+export class RandomizerModule {}
