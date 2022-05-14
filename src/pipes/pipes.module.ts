8d7ba47db047627c33b6f537ce6b521817d119ab
John sa Bahay <jfsdunglao.freelancing@gmail.com>
2022-05-14 08:07:18 +0800

    comment on pipe module

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from './validation.pipe';
 
+/**
+ * Note:
+ * reading of exception is from bottom to top. 
+ * the reading pipe direction is not sure. maybe the same.
+ */
 @Module({
     providers: [
         {
